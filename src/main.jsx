e74462a1d7b167dc209e00317d7a054ba79b5023
Elea Pimouguet <elea.pimouguet@gmail.com>
2024-06-13 19:02:06 +0200

    build: implement prettier

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,5 +10,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<App />} />
       </Routes>
     </Router>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
