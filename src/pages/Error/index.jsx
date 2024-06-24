b68f21fc6fd2e3a651400a9d39c6b157bfe166d5
Elea Pimouguet <elea.pimouguet@gmail.com>
2024-06-24 11:19:29 +0200

    build: implement Error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+import Error404 from '../../assets/error404.png';
+
 function Error() {
-  return <div>Error</div>;
+  return (
+    <section className="error">
+      <img src={Error404} alt="Erreur 404" className="error__image" />
+      <h2 className="error__title">
+        Oups ! La page que vous demandez n’existe pas.
+      </h2>
+      <Link to="/" className="error__link">
+        Retourner sur la page d’accueil
+      </Link>
+    </section>
+  );
 }
 
 export default Error;
